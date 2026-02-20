1b0c19106e19c0d7835118727672b0e8b3bcb668
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-02-20 08:49:21 -0300

    feat: create a new project about navigation in ViewCode - login and register screens

---
+++ Navegação ViewCode/Navegação ViewCode/UserInterface/Login/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Navegação ViewCode
+//
+//  Created by Gabriel Merenfeld on 20/02/26.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ Navegação ViewCode/Navegação ViewCode/UserInterface/Register/RegisterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterViewController.swift
+//  Navegação ViewCode
+//
+//  Created by Gabriel Merenfeld on 20/02/26.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
