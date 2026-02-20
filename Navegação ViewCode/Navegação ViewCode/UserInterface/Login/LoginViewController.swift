fab5f6d57260513de682b7f366f18c7523841bb9
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-02-20 09:04:48 -0300

    feat: initial configuration of ViewControllers

--- Navegação ViewCode/Navegação ViewCode/UserInterface/Login/LoginViewController.swift
+++ Navegação ViewCode/Navegação ViewCode/UserInterface/Login/LoginViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    private let contentView: UIView
+    
+    init(contentView: UIView) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
+    override func loadView() {
+        view = contentView
+    }
 }
--- Navegação ViewCode/Navegação ViewCode/UserInterface/Register/RegisterViewController.swift
+++ Navegação ViewCode/Navegação ViewCode/UserInterface/Register/RegisterViewController.swift
@@ -8,10 +8,19 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    private let contentView: UIView
+    
+    init(contentView: UIView) {
+        self.contentView = contentView
+        super.init(nibName: nil, bundle: nil)
     }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
+    override func loadView() {
+        view = contentView
+    }
 }
