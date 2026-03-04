ef861c0b00bd5903550f7b81cb91fabeb75c6917
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-03-04 07:41:36 -0300

    feat: created UserModel and UsersMock files

---
+++ ExercitandoTableView/ExercitandoTableView/TableView/Mocks/usersMock.swift
@@ -0,0 +1,15 @@
+//
+//  usersMock.swift
+//  ExercitandoTableView
+//
+//  Created by Gabriel Merenfeld on 04/03/26.
+//
+
+let usersMock: [UserModel] = [
+    UserModel(name: "Gabriel"),
+    UserModel(name: "Mariana"),
+    UserModel(name: "Pedro"),
+    UserModel(name: "Ana"),
+    UserModel(name: "Fernanda"),
+    UserModel(name: "Arthur"),
+]
---
+++ ExercitandoTableView/ExercitandoTableView/TableView/Models/UserModel.swift
@@ -0,0 +1,11 @@
+//
+//  UserModel.swift
+//  ExercitandoTableView
+//
+//  Created by Gabriel Merenfeld on 04/03/26.
+//
+
+struct UserModel {
+    let name: String
+    let icon: String = "person.fill"
+}
