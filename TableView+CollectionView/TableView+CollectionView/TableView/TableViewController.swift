fca1d376facdcbcccd15687c06e6612a1304731a
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-03-11 19:42:23 -0300

    feat: new TableView+CollectionView file

---
+++ TableView+CollectionView/TableView+CollectionView/TableView/TableViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TableView+CollectionView
+//
+//  Created by Gabriel Merenfeld on 11/03/26.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ TableView+CollectionView/TableView+CollectionView/TableView/TableViewScreen.swift
@@ -0,0 +1,15 @@
+//
+//  TableViewScreen.swift
+//  TableView+CollectionView
+//
+//  Created by Gabriel Merenfeld on 11/03/26.
+//
+
+import UIKit
+
+class TableViewScreen: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+    }
+}
