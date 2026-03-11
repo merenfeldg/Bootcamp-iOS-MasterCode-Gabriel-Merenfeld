f3638af1e8995c52156287782054ffdd7fea15bf
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-03-11 19:47:11 -0300

    feat: config TableViewScreen

--- TableView+CollectionView/TableView+CollectionView/TableView/TableViewScreen.swift
+++ TableView+CollectionView/TableView+CollectionView/TableView/TableViewScreen.swift
@@ -8,8 +8,40 @@
 import UIKit
 
 class TableViewScreen: UIView {
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundColor = .yellow
+        
+        return tableView
+    }()
     
     init() {
         super.init(frame: .zero)
+        setupView()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension TableViewScreen {
+    func setupView() {
+        addElements()
+        configConstraints()
+    }
+    
+    func addElements() {
+        addSubview(tableView)
+    }
+    
+    func configConstraints() {
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
     }
 }
