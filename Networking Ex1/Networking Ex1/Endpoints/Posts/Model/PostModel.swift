eeefe3033f840d88c14e11fc011c52ff6bf5a409
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-04-11 15:29:24 -0300

    feat: create createPost method in PostsService

--- Networking Ex1/Networking Ex1/Endpoints/Posts/Model/PostModel.swift
+++ Networking Ex1/Networking Ex1/Endpoints/Posts/Model/PostModel.swift
@@ -5,10 +5,9 @@
 //  Created by Gabriel Merenfeld on 11/04/26.
 //
 
-struct PostModel: Decodable {
-    let id: Int
-    let title: String
-    let body: String
+struct PostModel: Codable {
+    let id, userId: Int
+    let title, body: String
 }
 
 typealias PostList = [PostModel]
--- Networking Ex1/Networking Ex1/Endpoints/Posts/Service/PostsService.swift
+++ Networking Ex1/Networking Ex1/Endpoints/Posts/Service/PostsService.swift
@@ -127,4 +127,116 @@ final class PostsService {
         }
         task.resume()
     }
+    
+    static func createPost(_ newPost: PostModel, completion: @escaping (Result<Void, ErrorHandler>) -> Void) {
+        let urlString = "\(baseURL)/posts"
+        
+        guard let url = URL(string: urlString) else {
+            let error = ErrorHandler(message: "Erro ao criar a URL", statusCode: nil)
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        guard let dataBody = try? JSONEncoder().encode(newPost) else {
+            let error = ErrorHandler(message: "Erro ao serializar o JSON", statusCode: nil)
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = dataBody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                let error = ErrorHandler(message: "Erro ao realizar a requisição: \(error.localizedDescription)", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                let error = ErrorHandler(message: "Resposta inválida", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard (200..<300).contains(httpResponse.statusCode) else {
+                let error = ErrorHandler(message: "Erro ao realizar a requisição.\nSTATUS CODE: \(httpResponse.statusCode)", statusCode: httpResponse.statusCode)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        }
+        task.resume()
+    }
+    
+    static func replacePost(post: PostModel, completion: @escaping (Result<Void, ErrorHandler>) -> Void) {
+        let urlString = "\(baseURL)/posts/\(post.id)"
+        
+        guard let url = URL(string: urlString) else {
+            let error = ErrorHandler(message: "Erro ao criar a URL", statusCode: nil)
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        guard let dataBody = try? JSONEncoder().encode(post) else {
+            let error = ErrorHandler(message: "Erro ao serializar o JSON", statusCode: nil)
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.httpBody = dataBody
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error {
+                let error = ErrorHandler(message: "Erro ao realizar a requisição: \(error.localizedDescription)", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                let error = ErrorHandler(message: "Resposta inválida", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard (200..<300).contains(httpResponse.statusCode) else {
+                let error = ErrorHandler(message: "Erro ao realizar a requisição.\nSTATUS CODE: \(httpResponse.statusCode)", statusCode: httpResponse.statusCode)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        }
+        task.resume()
+    }
 }
