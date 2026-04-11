695adede25e08dcfda0539079963f71956fe63c7
Gabriel Merenfeld <gabrielmerenfeld@gmail.com>
2026-04-11 14:41:10 -0300

    feat: add fetchPostById method in PostsService

--- Networking Ex1/Networking Ex1/Endpoints/Posts/Service/PostsService.swift
+++ Networking Ex1/Networking Ex1/Endpoints/Posts/Service/PostsService.swift
@@ -66,5 +66,65 @@ final class PostsService {
                 }
             }
         }
+        task.resume()
+    }
+    
+    static func fetchPostById(_ id: Int, completion: @escaping (Result<PostModel, ErrorHandler>) -> Void) {
+        let urlString = "\(baseURL)/posts/\(id)"
+        
+        guard let url = URL(string: urlString) else {
+            let error = ErrorHandler(message: "Erro ao criar a URL", statusCode: nil)
+            DispatchQueue.main.async {
+                completion(.failure(error))
+            }
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error {
+                let error = ErrorHandler(message: "Erro ao realizar a requisição: \(error.localizedDescription)", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                let error = ErrorHandler(message: "Resposta inválida", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard (200..<300).contains(httpResponse.statusCode) else {
+                let error = ErrorHandler(message: "Erro ao realizar a requisição.\nSTATUS CODE: \(httpResponse.statusCode)", statusCode: httpResponse.statusCode)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let data else {
+                let error = ErrorHandler(message: "Nenhum dado foi retornado da requisição", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            do {
+                let post = try JSONDecoder().decode(PostModel.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(post))
+                }
+            } catch {
+                let error = ErrorHandler(message: "Error ao processar dados recebidos: \(error.localizedDescription)", statusCode: nil)
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
     }
 }
